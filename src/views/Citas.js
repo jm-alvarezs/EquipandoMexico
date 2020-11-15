fd68df70dca9c4836a55066c34cecaa8fbd0d6f9
jm-alvarezs <jm.alvarezs98@gmail.com>
2020-11-15 01:50:49 -0600

    added citas view

---
+++ src/views/Citas.js
@@ -0,0 +1,27 @@
+import React, {useContext, useEffect} from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import CitaCard from '../components/CitaCard';
+import {CitasContext} from '../context/CitasContext';
+
+const Citas = () => {
+  const {citas, getMisCitas} = useContext(CitasContext);
+
+  useEffect(() => {
+    getMisCitas();
+  }, []);
+
+  const renderCitas = () => {
+    if (citas && cotas !== null) {
+      return citas.map((cita) => <CitaCard key={cita.idCita} cita={cita} />);
+    }
+  };
+
+  return (
+    <View>
+      <Text>Citas</Text>
+      <ScrollView>{renderCitas()}</ScrollView>
+    </View>
+  );
+};
+
+export default Citas;
